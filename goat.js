08089a130c0be10381d399d9e005a644b5f0eaec
Kaitlyn Chan <ununoctium829@gmail.com>
2016-11-12 01:01:54 -0800

    goat js done

--- goat.js
+++ goat.js
@@ -21,6 +21,21 @@ function findKeyword(response){
       $('#messageInput').val('');
 }
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomResponses(){
+  var randResponses = ["I goat yo back", 
+                      "You goat owned!!", 
+                      "Calm yoself down, I goat this.", 
+                      "Aint nobody goat time for that. You have goat to be kidding me.", 
+                      "swagoat", 
+                      "Everybody tryin goat me"];
+  var index = getRandomInt(0,randResponses.length-1);
+  var rand = randResponses[index];
+  return rand;
+}
+
 
 function pushMessage(event){
   if (event.keyCode == 13){
@@ -36,34 +51,34 @@ function pushMessage(event){
             ||(text2.indexOf('salutation') >= 0)
             ||(text2.indexOf('address') >= 0)
             ||(text2.indexOf('yo') >= 0)){        
-      findKeyword('Hi! I ate lots of food this morning, but I feel peckish');
+      findKeyword('Who you gonnnna call??? GOATBUSTERS!');
     }else if((text2.indexOf('kill') >= 0)
             ||(text2.indexOf('wow') >= 0)
             ||(text2.indexOf('idc') >= 0)
             ||(text2.indexOf('rude') >= 0)
             ||(text2.indexOf('bored') >= 0)
             ||(text2.indexOf('hell') >= 0)){
-      findKeyword('Hey what’s up, chicken butt. Bach Bach Bach! You should listen to some Baroque');
+      findKeyword('You goat to block out the haters.');
     }else if((text2.indexOf('bye') >= 0)
             ||(text2.indexOf('bb') >= 0)
             ||(text2.indexOf('cya') >= 0)
             ||(text2.indexOf('sup') >= 0)){
-      findKeyword('Bock Bock!');
+      findKeyword('These puns have goatten out of hand!');
     }else if((text2.indexOf('work') >= 0)
             ||(text2.indexOf('fake') >= 0)
             ||(text2.indexOf('fail') >= 0)
             ||(text2.indexOf('stupid') >= 0)
             ||(text2.indexOf('bad') >= 0)
             ||(text2.indexOf('help') >= 0)){
-      findKeyword('Just wing it!');
-    }else if((text2.indexOf('travel') >= 0)
-            ||(text2.indexOf('road') >= 0)
-            ||(text2.indexOf('cross') >= 0)
+      findKeyword("Ain't nobody goat time for that");
+    }else if((text2.indexOf('help') >= 0)
+            ||(text2.indexOf('i hate') >= 0)
+            ||(text2.indexOf('you suck') >= 0)
             ||(text2.indexOf('why') >= 0)){
-      findKeyword('Here’s a lesson I learned when I traveled: never be that chicken who crossed the street…');
+      findKeyword(' I ain’t no scapegoat.');
     }
     else{
-      findKeyword('yo');
+      findKeyword(randomResponses());
     }
   }
 }
