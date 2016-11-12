e29193360a139671bfa351516458cf6728561b00
Kaitlyn Chan <ununoctium829@gmail.com>
2016-11-12 00:44:51 -0800

    hiya

--- main.js
+++ main.js
@@ -20,6 +20,11 @@ function findKeyword(response){
       chatData.push({name: name, text: text});
       $('#messageInput').val('');
 }
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function randomResponses(){
   var randResponses = ["Deja Moo", 
                       "You are udder chaos", 
@@ -47,6 +52,7 @@ function pushMessage(event){
     $('#messageInput').val('');
     var text2 = text.trim();
 
+
     if (text2.indexOf('trump') >= 0 ){
       findKeyword('Moo-ve to Canada');
     }else if(text2.indexOf('unicorn') >= 0){
@@ -58,9 +64,44 @@ function pushMessage(event){
             ||(text2.indexOf('salutation') >= 0)
             ||(text2.indexOf('address') >= 0)
             ||(text2.indexOf('yo') >= 0)){
-            
-    
       findKeyword('Moo there!! *cow greets you*');
+    }else if(text2.indexOf('bye') >= 0 
+            ||(text2.indexOf('bb') >= 0)){
+      findKeyword('Have a nice moo day!');
+    }else if(text2.indexOf('again') >= 0){
+      findKeyword("deja moo");
+    }else if(text2.indexOf('math') >= 0){
+      findKeyword("baby cows go to lunch in the calf-ateria");
+    }else if(text2.indexOf('travel') >= 0){
+      findKeyword("Moo-York");
+    }else if(text2.indexOf('coffee') >= 0){
+      findKeyword("I like my coffee decalfinated!");
+    }else if((text2.indexOf('awesome') >= 0)
+            ||(text2.indexOf('spectacular')>=0)
+            ||(text2.indexOf('great') >= 0)
+            ||(text2.indexOf('wonderful')>=0)
+            ||(text2.indexOf('good')>= 0)
+            ||(text2.indexOf('nice')>= 0)
+            ||(text2.indexOf('af')>=0)){
+      findKeyword("That's moo-gnificent");
+    }else if((text2.indexOf('ugly') >= 0)
+            ||(text2.indexOf('bad')>=0)
+            ||(text2.indexOf('tired')>=0)
+            ||(text2.indexOf('gross'))>=0){
+      findKeyword("That's terrible! Be more moo-gnificent like me!!");
+    }else if((text2.indexOf('fail') >= 0)
+            ||(text2.indexOf('failing')>=0)
+            ||(text2.indexOf('dumb')>=0)
+            ||(text2.indexOf('stupid')>=0)
+            ||(text2.indexOf('help')>=0)
+            ||(text2.indexOf('dying')>=0)
+            ||(text2.indexOf('bad')>=0)
+            ||(text2.indexOf('suck')>=0)
+            ||(text2.indexOf('doomed')>=0)){
+      findKeyword("Just keep moo-ving.");
+    }else if((text2.indexOf('beautiful') >= 0)
+            ||(text2.indexOf('pretty'))>=0){
+      findKeyword("S-mooch *cow licks you*");
     }else{
       findKeyword(randomResponses());
     }
